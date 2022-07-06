084c2fb725a250932318b5400184118509ba8751
Deya Omondi <henryomondi@icloud.com>
2022-07-05 19:45:38 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,27 @@
-nclude <stdio.h>
-#include "main.h"
-/**
- * *main - Entry point
- * *
- * *Return: Always 0 (success)
- * */
-int main(void)
+#include <unistd.h>
+
+int _putchar(char c)
 {
-		char q="_putchar";
-			_putchar(q);
-				_putchar('\n');
-
-
-					return (0);
+		return (write(1, &c, 1));
 }
+
+int main ()
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+
+	return 0;
+
+
+}
+
+
