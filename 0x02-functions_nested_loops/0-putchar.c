4f9e402ae91b87313ec5b77969078a7133bb591e
Deya Omondi <henryomondi@icloud.com>
2022-07-05 19:39:41 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+nclude <stdio.h>
+#include "main.h"
+/**
+ * *main - Entry point
+ * *
+ * *Return: Always 0 (success)
+ * */
+int main(void)
+{
+		char q="_putchar";
+			_putchar(q);
+				_putchar('\n');
+
+
+					return (0);
+}
