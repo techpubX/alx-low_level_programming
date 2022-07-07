15128616378ba06070335ef5f59a9c574703c785
Deya Omondi <henryomondi@icloud.com>
2022-07-06 18:57:04 -0700

    low level programming

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
- *  * print_alphabet - function to print abc
- *   *
- *    * Return: 0
- *     */
+ *  * main - check the code
+ *   * print_alphabet - print alphabet lowercase
+ *    *
+ *     * Return: Always 0.
+ *      */
 void print_alphabet(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-			_putchar(c);
-		}
-_putchar('\n');
+		_putchar(alphabet);
+	}
+	_putchar('\n');
 }
