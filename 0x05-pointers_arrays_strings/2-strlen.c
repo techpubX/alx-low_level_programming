4d525f14a41cd4d41b82f1574b17413d706d021f
Deya Omondi <henryomondi@icloud.com>
2022-07-11 02:41:20 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* len returns the length
+ * *for counts
+ * * Retunr the length
+ * */
+
+int main() {
+	    char len[] = "omondi";
+	        int i;
+
+		    for (i = 0; len[i] != '\0'; ++i);
+		        
+		        printf("%d", i);
+			    return 0;
+}
+
