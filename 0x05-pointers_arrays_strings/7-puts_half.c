dbc26f9beb89eb656b2100939556b190f14b813b
Deya Omondi <henryomondi@icloud.com>
2022-07-11 06:12:36 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *   * do_half - Prints half of the string
+ *     * @str: The string to print
+ *       *
+ *         * Return: void
+ *           */
+void do_half(char *str)
+{
+		int j = 0;
+			int k;
+
+				while (str[j] != '\0')
+						{
+									j++;
+										}
+
+					if (j % 2 == 1)
+							{
+										k = (j - 1) / 2;
+												k += 1;
+													}
+						else
+								{
+											k = j / 2;
+												}
+
+							for (; k < j; k++)
+									{
+												_putchar(str[k]);
+													}
+
+								_putchar('\n');
+}
