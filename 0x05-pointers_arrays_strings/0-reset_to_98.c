52d3c488b059bb22090bd9e56e094dc99fc1a4d2
Deya Omondi <henryomondi@icloud.com>
2022-07-11 02:15:16 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,8 @@
 #include "main.h"
 
-/**
- *   * void reset_to_98(int *n)
- *     *
- *       * Return: void
- *        
-*/
+/*void reset_to_98(int *n)*/
+
 void reset_to_98(int *n)
 {
-
-
 		*n = 98;
 }
