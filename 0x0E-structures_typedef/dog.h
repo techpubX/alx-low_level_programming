21dceef1bca839aa9d5ee5d33385a586e14c7906
Deya Omondi <henryomondi@icloud.com>
2022-08-01 11:47:58 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,22 +2,27 @@
 #define _DOG_H_
 
 /**
- *  * struct dog - struct with dog info
- *   * @name: First member
- *    * @age: Second member
- *     * @owner: Third member
- *      *
- *       * Description: store different data types
- *        */
-typedef struct dog
+ *  * struct dog - information about dogs
+ *   * @name: name of dog
+ *    * @age: age of dog
+ *     * @owner: owner of dog
+ *      */
+struct dog
 {
 		char *name;
 			float age;
 				char *owner;
-} dog_t;
+};
 
+/**
+ *  * dog_t - Typedef for struct dog
+ *   */
+typedef struct dog dog_t;
+
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+#endif /* _DOG_H_ */
